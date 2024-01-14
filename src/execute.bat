dfc4748033835f210248ca6b330829cb0cc370e3
matheusangelo <matheusangelo10@gmail.com>
2024-01-14 19:55:32 -0300

    init

--- src/execute.bat
+++ src/execute.bat
@@ -1,2 +1,11 @@
-aws cloudformation deploy --stack-name cfn-stack --template-file "cfn.yaml"  --endpoint-url http://localhost:4566
-aws cloudformation delete-stack --stack-name cfn-stack --endpoint-url http://localhost:4566
+echo "create s3"
+aws cloudformation deploy --stack-name s3-stack --template-file "./iac/s3.yaml"  --endpoint-url http://localhost:4566
+
+echo "deploy code to s3"
+aws s3 cp ./iac/app.zip s3://app/
+
+echo "deploy stack"
+aws cloudformation deploy --stack-name app-stack --template-file "./iac/main.yaml"  --endpoint-url http://localhost:4566
+
+
+aws lambda invoke --function-name MyLambdaFunction --payload '{"key": "value"}' output.json
